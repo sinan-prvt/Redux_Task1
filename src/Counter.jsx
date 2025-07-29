d3219763f12823dcbe66e63465783406633c837e
sinan-prvt <mohamedsinan9400@gmail.com>
2025-07-29 20:05:31 +0530

    Add full Redux counter app

---
+++ src/Counter.jsx
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from "react-redux"
+
+const Counter = () => {
+    const count = useSelector((state) => state.count)
+    const dispatch = useDispatch()
+
+    return(
+        <div>
+            <h2>Count: {count}</h2>
+            <button onClick={() => dispatch({ type: 'Increment' })}>+</button>
+            <button onClick={() => dispatch({ type: 'Decrement' })} >-</button>
+            <button onClick={() => dispatch({ type: 'Reset' })} >Reset</button>
+        </div>
+    )
+}
+
+export default Counter
---
+++ src/store.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux';
+
+const initialState = {
+    count:0
+} 
+
+function counterReducer(state = initialState , action){
+    switch(action.type) {
+        case 'Increment':
+            return { count: state.count + 1 }
+        case 'Decrement':
+            return { count: state.count - 1 }
+        case 'Reset':
+            return { count: 0 }
+        default :
+            return state
+    }
+}
+
+const store = createStore(counterReducer)
+
+export default store
